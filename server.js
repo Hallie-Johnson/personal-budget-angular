b47d46bcc6abfb429d6834bf590724ffba014d64
Hallie-Johnson <HEJohnson130@gmail.com>
2024-09-12 14:31:53 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,24 +4,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const fs = require('fs');
+const path = require('path');
+
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 375
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+app.get('/budget', (req, res) => {
+    const filePath = path.join(__dirname, 'budget-data.json');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading budget data:', err);
+            res.status(500).send('Server Error');
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
+});
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
@@ -34,21 +32,3 @@ app.get('/budget', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
-/*const cors = require('cors');
-const app = express();
-const port = 3000;
-
-app.use(cors());
-
-
-
-
-app.get('/budget', (req, res) => {
-    res.json(budget);
-});
-
-app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
-}); */
